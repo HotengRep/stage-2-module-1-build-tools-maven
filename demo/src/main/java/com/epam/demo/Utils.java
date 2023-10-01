baea72c764a0e592dd4fd41628221c7eed4f5ce0
Makar <hoteng789@gmail.com>
2023-10-01 13:07:19 +0400

    Work with Maven

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,17 @@
 package com.epam.demo;
 
 import java.util.List;
+import com.epam.utils.StringUtils;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+        try {
+            return args.stream().allMatch(StringUtils::isPositiveNumber);
+        }
+        catch (NullPointerException exception)
+        {
+            return  false;
+        }
+
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,13 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        try {
+            return Double.parseDouble(str) > 0;
+        }
+        catch (NumberFormatException exception)
+        {
+            return  false;
+        }
+
     }
 }
